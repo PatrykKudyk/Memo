37305895539231bf48de50172f07cc1db23956c7
Papryk <gustaw5022@gmail.com>
2020-06-14 13:26:34 +0200

    added fragment + changed menu animations

--- Memo/app/src/main/java/com/example/memo/MainActivity.kt
+++ Memo/app/src/main/java/com/example/memo/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.memo
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.tictactoe.fragments.*
+import com.example.memo.fragments.*
 
 class MainActivity : AppCompatActivity(),
     BigGameFragment.OnFragmentInteractionListener,
     MainMenuFragment.OnFragmentInteractionListener,
     MediumGameFragment.OnFragmentInteractionListener,
     SizeChoiceFragment.OnFragmentInteractionListener,
-    SmallGameFragment.OnFragmentInteractionListener {
+    SmallGameFragment.OnFragmentInteractionListener,
+    RulesFragment.OnFragmentInteractionListener,
+    GameSizeFragment.OnFragmentInteractionListener {
 
     lateinit var mainMenuFragment: MainMenuFragment
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +22,10 @@ class MainActivity : AppCompatActivity(),
 
         supportFragmentManager
             .beginTransaction()
+            .setCustomAnimations(
+                R.anim.enter_bottom_to_top, R.anim.exit_top_to_bottom,
+                R.anim.enter_top_to_bottom, R.anim.exit_bottom_to_top
+            )
             .add(R.id.frame_layout, mainMenuFragment)
             .commit()
     }
--- Memo/app/src/main/java/com/example/memo/fragments/GameSizeFragment.kt
+++ Memo/app/src/main/java/com/example/memo/fragments/GameSizeFragment.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import com.example.memo.R
 
 
@@ -24,15 +23,14 @@ private const val ARG_PARAM2 = "param2"
  * Use the [AccountFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainMenuFragment : Fragment() {
+class GameSizeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
 
     private lateinit var rootView: View
-    private lateinit var newGameButton: Button
-    private lateinit var rulesButton: Button
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +46,7 @@ class MainMenuFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        rootView = inflater.inflate(R.layout.fragment_main_menu, container, false);
+        rootView = inflater.inflate(R.layout.fragment_game_size, container, false);
         initFragment()
         return rootView
     }
@@ -79,40 +77,13 @@ class MainMenuFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() =
-            MainMenuFragment().apply {
+            GameSizeFragment().apply {
                 arguments = Bundle().apply {
                 }
             }
     }
 
     private fun initFragment() {
-        newGameButton = rootView.findViewById(R.id.button_new_game)
-        rulesButton = rootView.findViewById(R.id.button_rules)
 
-        newGameButton.setOnClickListener {
-            val gameSizeFragment = GameSizeFragment.newInstance()
-            fragmentManager
-                ?.beginTransaction()
-                ?.setCustomAnimations(
-                    R.anim.enter_left_to_right, R.anim.exit_right_to_left,
-                    R.anim.enter_right_to_left, R.anim.exit_left_to_right
-                )
-                ?.replace(R.id.frame_layout, gameSizeFragment)
-                ?.addToBackStack(gameSizeFragment.toString())
-                ?.commit()
-        }
-
-        rulesButton.setOnClickListener {
-            val rulesFragment = RulesFragment.newInstance()
-            fragmentManager
-                ?.beginTransaction()
-                ?.setCustomAnimations(
-                    R.anim.enter_left_to_right, R.anim.exit_right_to_left,
-                    R.anim.enter_right_to_left, R.anim.exit_left_to_right
-                )
-                ?.replace(R.id.frame_layout, rulesFragment)
-                ?.addToBackStack(rulesFragment.toString())
-                ?.commit()
-        }
     }
 }
